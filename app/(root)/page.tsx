c69be776a48f75091ae984426b595b15e771e9c3
Tirth <tirthsavalia@gmail.com>
2025-04-17 16:41:00 +0530

    show generated interview section with bifurcation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,8 +4,21 @@ import Image from 'next/image'
 import React from 'react'
 import { dummyInterviews } from '@/constants'
 import InterviewCard from '@/constants/InterviewCard'
+import { getCurrentUser, getInterviewsByUserId, getLatestInterviews } from '@/lib/actions/auth.action'
 
-const page = () => {
+const page =async () => {
+  const user = await getCurrentUser();
+
+  const [userInterviews, latestInterviews] = await Promise.all([
+    await getInterviewsByUserId(user?.id!),
+    await getLatestInterviews({userId: user?.id!})
+  ])
+
+
+
+  const hasPastInterviews = userInterviews?.length > 0;
+  const hasUpcomingInterviews = latestInterviews?.length > 0;
+
   return (
     <>
       <section className='card-cta'>
@@ -28,19 +41,29 @@ const page = () => {
       <section className='flex flex-col gap-6 mt-8'>
       <h2>Your Interviews</h2>
       <div className='interviews-section'>
-        {dummyInterviews.map((interview)=>(
-          <InterviewCard {...interview} key={interview.id} />
-        ))}
-        {/* <p>You haven't taken any interviews.</p> */}
+        {
+          hasPastInterviews ? (
+            userInterviews?.map((interview) => (
+              <InterviewCard {...interview} key={interview.id}/>
+            ))) : (
+              <p>You haven't taken any interviews.</p>
+            )
+        }
+
       </div>
       </section>
     
       <section className='flex flex-col gap-6 mt-8'>
         <h2>Take an Interview</h2>
         <div className='interviews-section'>
-        {dummyInterviews.map((interview)=>(
-          <InterviewCard {...interview} key={interview.id} />
-        ))}
+        {
+          hasUpcomingInterviews ? (
+            latestInterviews?.map((interview) => (
+              <InterviewCard {...interview} key={interview.id}/>
+            ))) : (
+              <p>There are no new interviews available.</p>
+            )
+        }
 
         </div>
       </section>
