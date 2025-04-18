a438df5e2bee1cfccd4f6be66b0708e37345dbc9
Tirth <tirthsavalia@gmail.com>
2025-04-18 23:21:15 +0530

    implement view interview session

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,10 @@ import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
-import { dummyInterviews } from '@/constants'
 import InterviewCard from '@/constants/InterviewCard'
-import { getCurrentUser, getInterviewsByUserId, getLatestInterviews } from '@/lib/actions/auth.action'
+import { getCurrentUser } from '@/lib/actions/auth.action'
+import { getInterviewsByUserId, getLatestInterviews } from '@/lib/actions/general.action'
+
 
 const page =async () => {
   const user = await getCurrentUser();
--- constants/InterviewCard.tsx
+++ constants/InterviewCard.tsx
@@ -6,7 +6,7 @@ import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import DisplayTechIcons from '@/components/DisplayTechIcons';
 
-const InterviewCard = ({interviewId, userId, role, type, techstack, createdAt}: InterviewCardProps) => {
+const InterviewCard = ({id, userId, role, type, techstack, createdAt}: InterviewCardProps) => {
     const feedback = null as Feedback | null;
     const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
     const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM D,YYYY')
@@ -39,7 +39,7 @@ const InterviewCard = ({interviewId, userId, role, type, techstack, createdAt}: 
             <div className='flex flex-row justify-between'>
                 <DisplayTechIcons techStack={techstack}/>
                 <Button className='btn-primary'>
-                    <Link href={feedback? `/interview/${interviewId}/feedback` : `/interview/${interviewId}`}>
+                    <Link href={feedback? `/interview/${id}/feedback` : `/interview/${id}`}>
                     {feedback? 'Check Feedback' : 'View Interview'}
                     </Link>
                 </Button>
